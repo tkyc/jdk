4939a9100ec45c2f4e98e9188c9578140ccf124f
Terry Chow <v-terrychow@microsoft.com>
2023-05-16 17:38:26 -0700

    Test update pt.3

--- test/jdk/java/nio/channels/etc/PrintSupportedOptions.java
+++ test/jdk/java/nio/channels/etc/PrintSupportedOptions.java
@@ -37,6 +37,8 @@ import java.util.*;
 
 import jdk.test.lib.net.IPSupport;
 
+import static jdk.net.ExtendedSocketOptions.TCP_SIO_KEEPALIVE;
+
 public class PrintSupportedOptions {
 
     @FunctionalInterface
@@ -63,11 +65,13 @@ public class PrintSupportedOptions {
         try (T ch = supplier.get()) {
             System.out.println(ch);
             for (SocketOption<?> opt : ch.supportedOptions()) {
-                Object value = ch.getOption(opt);
-                System.out.format(" %s -> %s%n", opt.name(), value);
-                if (!READ_ONLY_OPTS.contains(opt.name())) {
-                    if (value != null)
-                        ch.setOption((SocketOption<Object>) opt, value);
+                if (!opt.equals(TCP_SIO_KEEPALIVE)) { // can't get
+                    Object value = ch.getOption(opt);
+                    System.out.format(" %s -> %s%n", opt.name(), value);
+                    if (!READ_ONLY_OPTS.contains(opt.name())) {
+                        if (value != null)
+                            ch.setOption((SocketOption<Object>) opt, value);
+                    }
                 }
             }
         }
